be90f358d6f87aa8adcd6c128b40244d19edef2d
Gabriela Zalta <gzalta96@gmail.com>
2020-10-31 09:25:42 -0500

    password parameters set

--- script.js
+++ script.js
@@ -6,17 +6,26 @@ var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "`", "{", "|", "}", "~"];
 var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var confirmLength = "";
-
 function generatePassword (){
   var confirmLength = (prompt ("How many characters would you like your password to have? Please enter a number between 8 and 128"));
   
   if (confirmLength >= 8 && confirmLength <= 128) {
     alert("Your password will contain " + confirmLength + " characters.");
   } else {
-  alert("You entered an invalid response. Please try again");
+  alert("You entered an invalid response. Please try again.");
   return generatePassword();
   }
+    var confirmLowerChar = (confirm ("Would you like to include lower case letters in your password?"));
+    var confirmUpperChar = (confirm ("Would you like to include upper case letters in your password?"));
+    var confirmSpecialChar = (confirm ("Would you like to include special characters in your password?"));
+    var confirmNumChar = (confirm ("Would you like to include numbers in your password?"));
+
+    if (confirmLowerChar === false && confirmUpperChar === false && confirmSpecialChar === false && confirmNumChar === false) {
+    alert("You must select at least one parameter. Please try again.");
+    return generatePassword();
+    } else {
+      alert ("You have set the password parameters succesfully, click OK to view your password.")
+    }
 };
 
 generatePassword ();
